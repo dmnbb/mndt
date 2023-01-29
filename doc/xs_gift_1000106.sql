a4a3cd98d60f398ebb8ff9b166a4cf3c62f4813d
dev_server <you@example.com>
2023-01-29 12:15:40 +0800

    mysql

---
+++ doc/xs_gift_1000106.sql
@@ -0,0 +1,47 @@
+-- MySQL dump 10.13  Distrib 5.7.32, for Linux (x86_64)
+--
+-- Host: localhost    Database: xianshi
+-- ------------------------------------------------------
+-- Server version	5.7.32-0ubuntu0.18.04.1-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED='300ca9ea-4369-11eb-a19a-00163e0879f0:1-97506079';
+
+--
+-- Dumping data for table `xs_gift`
+--
+-- WHERE:  id=1000106
+
+LOCK TABLES `xs_gift` WRITE;
+/*!40000 ALTER TABLE `xs_gift` DISABLE KEYS */;
+INSERT INTO `xs_gift` VALUES (1000106,'','長壽麵','Longevity Noddle','نودلز طول العمر','Mee Panjang Umur','บะหมี่อายุยืน','Mie Panjang Umur','Mỳ trường thọ','장수면',0.00,'','room,chat','','normal','normal','money',1,0,0,0,1,0,'','','multiframe','','','2','','',0,'normal',339068,0,0,0,0,0,0,0,'',5,0,'normal','長寿麵',-1,0);
+/*!40000 ALTER TABLE `xs_gift` ENABLE KEYS */;
+UNLOCK TABLES;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-01-29 12:14:52
